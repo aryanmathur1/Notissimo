836b380325ca9ff364b33c87c8fad1b2ac76f006
aryanmathur1 <134450794+aryanmathur1@users.noreply.github.com>
2025-01-17 10:42:01 -0500

    v0.0.1
    test if repo is working

---
+++ NotissimoApp/src/Notissimo/UtilityValues.java
@@ -0,0 +1,9 @@
+package Notissimo;/*
+ * THESE ARE ALL THE CONSTANTS IN OUR PROJECT
+ */
+
+public class UtilityValues {
+
+    public static final String TEST_STRING = "Test if Commited + Packages";
+
+}
---
+++ NotissimoApp/src/Notissimo/testFiles/TestCommitPackage.java
@@ -0,0 +1,11 @@
+package Notissimo.testFiles;
+
+import Notissimo.UtilityValues;
+
+public class TestCommitPackage {
+
+    public static void main(String[] args) {
+        System.out.println(UtilityValues.TEST_STRING);
+    }
+
+}
