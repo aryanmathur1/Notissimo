173a116da7f80f38434fc4c9eb79bd5aff29635e
aryanmathur1 <134450794+aryanmathur1@users.noreply.github.com>
2025-02-25 11:53:05 -0500

    v0.6
    custom button

--- NotissimoApp/src/Notissimo/views/CustomElements/MyButton.java
+++ NotissimoApp/src/Notissimo/views/CustomElements/MyButton.java
@@ -2,6 +2,8 @@ package Notissimo.views.CustomElements;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MyButton extends JButton {
 
@@ -13,7 +15,45 @@ public class MyButton extends JButton {
     private int radius = 0;
 
     public MyButton() {
+        // init color
+        color = Color.WHITE;
+        colorOver = new Color(183, 160, 250);
+        colorClick = new Color(155, 144, 184);
+        borderColor = new Color(60, 30, 136);
+
         setContentAreaFilled(false);
+
+        // add event mouse
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                setBackground(colorOver);
+                over = true;
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+                setBackground(color);
+                over = false;
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+                setBackground(colorClick);
+                over = true;
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+                if (over) {
+                    setBackground(colorOver);
+                } else {
+                    setBackground(color);
+                }
+            }
+
+        });
     }
 
     public boolean isOver() {
@@ -68,7 +108,14 @@ public class MyButton extends JButton {
     protected void paintComponent(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
-        
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // paint border
+        g2d.setColor(borderColor);
+        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2d.setColor(getBackground());
+        // border set 2 pixels
+        g2d.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, radius, radius);
 
         super.paintComponent(g);
 
